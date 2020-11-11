30963e0ded876e4d5851809c8b6289e10d9cd6b8
Daniil Komin <daniilkom1n@yandex.ru>
2020-11-12 02:26:53 +0300

    add environment variable for local backend

--- src/plugins/baseURL.js
+++ src/plugins/baseURL.js
@@ -1,3 +1,6 @@
 import Vue from "vue";
 // base url for REST api
-Vue.prototype.$baseURL = "http://167.99.252.163/api/memes/";
+
+Vue.prototype.$baseURL = process.env.VUE_APP_API_URL == undefined ?
+                                                            "http://167.99.252.163/api/memes/"
+                                                            : process.env.VUE_APP_API_URL;
