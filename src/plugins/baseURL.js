4ff99f825172148e50fd183d42873528662bfcd9
Daniil Komin <daniilkom1n@yandex.ru>
2020-11-12 22:30:40 +0300

    add docker environment variable, remove vue env files

--- src/plugins/baseURL.js
+++ src/plugins/baseURL.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 // base url for REST api
+console.log(process.env.VUE_APP_API_URL);
 
 Vue.prototype.$baseURL = process.env.VUE_APP_API_URL == undefined ?
                                                             "http://167.99.252.163/api/memes/"
