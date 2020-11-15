6e86e6c5b2eca76d6cfb64c2c1ef624751748538
Daniil Komin <daniilkom1n@yandex.ru>
2020-11-15 22:09:21 +0300

    add base layouts

---
+++ src/views/main-view.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div
+    search-bar
+    search-result
+</template>
+
+<script>
+import searchBar from '../components/SearchBar';
+import searchResult from '../components/SearchResult';
+export default {
+  name: 'main-view',
+  components: {
+    searchBar,
+    searchResult,
+  },
+  data: () => ({
+
+  }),
+};
+</script>
---
+++ src/views/user-view.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    h1.text-center Мои мемесы
+</template>
+
+<script>
+export default {
+  name: 'user-view',
+  components: {
+
+  },
+  data: () => ({
+
+  }),
+};
+</script>
